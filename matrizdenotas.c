27287c6bde11c9b23a020c6cad8ef2e9df36aa97
taianeds <55223618+taianeds@users.noreply.github.com>
2019-10-28 23:58:12 -0300

    Update matrizdenotas.c

--- matrizdenotas.c
+++ matrizdenotas.c
@@ -1,12 +1,13 @@
 /*****************************************************************************
 
-ler o resultado e apresentar matriz ARRUMAR
-
+Criar um programa que tenha uma matriz 5x4. Cada linha dessa matriz representa um aluno e cada coluna a sua nota.
+apos, o programa devera apresentar a media de cada aluno, mostrar qual foi a menor nota (e de que aluno) e qual foi a maior nota (e qual o aluno)
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
+//definicao de variaveis
     int i;
     int j;
     float temp;
@@ -17,21 +18,17 @@ int main()
     float matriz [numeroalunos][numeronotas];
     float vetormedia [5];
 
-    // [0][][][][]
-    // [1][][][][]
-    // [2][][][][]
-    // [3][][][][]
-    
+//coleta de notas e armazenamento na matriz
     for(i=0; i<numeroalunos; i++)
     {
         for(j=0; j<numeronotas; j++)
         {
-            printf("\ndigite a nota %d do aluno %d:", j+1, i+1);
+            printf("\ndigite a nota %d do aluno %d:  ", j+1, i+1);
             scanf("%f", &temp);
-            // printf ("\n");
-            // printf("%d", temp);
 
             matriz [i][j] = temp;
+
+// print para entender a interacao da matriz (nao necessario no codigo para o resultado final )
 
             printf("\n===================\n");
             int v,t,r,c;
@@ -46,6 +43,8 @@ int main()
                 printf("\n");  
             }
         }
+
+//media das notas coletadas
         float nota=0;
         int posicao=0;
         float media=0;
@@ -53,17 +52,39 @@ int main()
             nota+=matriz[i][posicao];
         }
         media=nota/numeronotas;
-        printf("media do aluno %d eh: %.2f\n", i+1, media);
 
         vetormedia[i] = media;
 
     }
+//armazenamento das medias
     int aluno=0;
     for (aluno=0; aluno<numeroalunos; aluno++)
     {
-        printf ("a media do aluno %d eh %.2f\n", aluno+1, vetormedia[aluno]);
+        printf ("\na media do aluno %d eh %.2f\n", aluno+1, vetormedia[aluno]);
     }
-    
+//classificacao da maior e menor media 
+    int posicaoaluno;
+    int posicaomenor=0;
+    float menornota=999;
+    float maiornota=0;
+    int posicaomaior=0;
+    for (posicaoaluno=0; posicaoaluno<numeroalunos; posicaoaluno++)
+    {
+        if(vetormedia[posicaoaluno]<menornota)
+        {
+            menornota=vetormedia[posicaoaluno];
+            posicaomenor=posicaoaluno;
+        }
+        if(vetormedia[posicaoaluno]>maiornota)
+        {
+            maiornota=vetormedia[posicaoaluno];
+            posicaomaior=posicaoaluno;
+        }
+    }
+//apresentacao do resultado final
+    printf ("\na menor media eh %.2f do aluno %d\n", menornota, posicaomenor+1);
+    printf ("a maior media eh %.2f do aluno %d", maiornota, posicaomaior+1);
+
     return 0;
         
 }
