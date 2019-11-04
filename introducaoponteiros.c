5b54fe686ec78fbf947c72df02f54ad3d8ed2c22
taianeds <55223618+taianeds@users.noreply.github.com>
2019-11-04 20:43:10 -0300

    Update introducaoponteiros.c

--- introducaoponteiros.c
+++ introducaoponteiros.c
@@ -30,3 +30,34 @@ int main()
 	//system("PAUSE");
 	return 0;
 }
+
+/******************************************************************************
+quando tem * ele tem permissão para entrar na casa
+quando nao tem o * ele só mostra onde é a casa
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	//Declaração de variáveis
+	int y;
+	int *yPtr;
+	
+	//Inicialização de variáveis
+    
+    printf ("digite um numero ");
+    scanf ("%d",&y);
+	yPtr = &y;	//A variável aPtr recebe o "endereço" de "y"
+	
+	printf("O endereco de y eh %p\n", yPtr);
+			
+	printf("O valor de y eh %d\n", *yPtr);
+			
+	printf("ja o endereco do yPtr eh %p", &yPtr);
+			
+	//system("PAUSE");
+	return 0;
+}
